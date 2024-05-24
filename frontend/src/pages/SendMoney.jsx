5991abbc13a57f7f7a8973983070f4affac65598
Komal Singh <komalsingh@Komals-MacBook-Air.local>
2024-05-24 21:49:00 +0530

    Navigate to dashboard on successful transaction.

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,8 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 
 export const SendMoney = () => {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const id = searchParams.get("id");
   const name = searchParams.get("name");
@@ -42,19 +43,23 @@ export const SendMoney = () => {
               </div>
               <button
                 onClick={() => {
-                  axios.post(
-                    "http://localhost:3000/api/v1/account/transfer",
-                    {
-                      to: id,
-                      amount,
-                    },
-                    {
-                      headers: {
-                        Authorization:
-                          "Bearer " + localStorage.getItem("token"),
+                  axios
+                    .post(
+                      "http://localhost:3000/api/v1/account/transfer",
+                      {
+                        to: id,
+                        amount,
                       },
-                    }
-                  );
+                      {
+                        headers: {
+                          Authorization:
+                            "Bearer " + localStorage.getItem("token"),
+                        },
+                      }
+                    )
+                    .then(() => {
+                      navigate("/dashboard");
+                    });
                 }}
                 class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
               >
